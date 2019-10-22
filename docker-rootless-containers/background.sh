19eb827653418cbe1af20ac030d698c3b1bcf552
Andoni A <14891798+andoniaf@users.noreply.github.com>
2019-10-22 13:41:18 +0200

    Add scenario setup script

--- docker-rootless-containers/background.sh
+++ docker-rootless-containers/background.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 USER=mindundi
+FILE=Dockerfile
 
-useradd -m $USER
+# Inside Katakoda env we can only work under /root
+chmod 755 /root
+
+useradd -m -b /root $USER
+usermod -G docker $USER
+
+mv $FILE /root/$USER/ 
+chown mindundi:mindundi /root/$USER/$FILE
